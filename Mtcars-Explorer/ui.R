3b75c2dc82ef5b9db9590884976d87dc0cf19301
WPShen53 <WPShen53@users.noreply.github.com>
2016-04-12 12:28:36 +0800

    Update ui.R

--- Mtcars-Explorer/ui.R
+++ Mtcars-Explorer/ui.R
@@ -14,7 +14,7 @@ dispName <- c('Weight (1000 lbs)'='wt',
               'Displacement (cu.in.)'='disp',
               'Gross horsepower'='hp',
               'Rear axle ratio'='drat',
-               '1/4 mile time'='qsec',
+              '1/4 mile time'='qsec',
               'V/S'='vs',
               'Number of forward gears'='gear',
               'Number of carburetors'='carb')
@@ -22,27 +22,38 @@ dispName <- c('Weight (1000 lbs)'='wt',
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Linear Regression Analysis of Miles/(US) gallon"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-        selectInput('x', 'Regressor', dispName),
-        radioButtons("am", label = "Transmission",
-                     choices = list("Automatic"=0, "Manual"=1),
-                     selected = 1),
-        checkboxGroupInput("cyl", label = "Number of Cyclinders",
-                     choices = list("4"=4, "6"=6, "8"=8),
-                     selected = 4)
-#        radioButtons("cyl", label = "Number of Cyclinders",
-#                     choices = list("4"=4, "6"=6, "8"=8),
-#                     selected = 4)
-    ),
     
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
+    # Application title
+    titlePanel("Linear Regression Analysis for the Gas Milage"),
+    
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            wellPanel(
+                strong("Instruction"),br(),br(),
+                'This application uses the Motor Trend Car Road 
+                Tests (mtcars) dataset. You can pick the variables from the panel below, 
+                and view the impact on gase mileage of popular cars and 
+                the data used.'
+            ),
+            wellPanel(
+                selectInput('x', 'Regressor', dispName),
+                radioButtons("am", label = "Transmission",
+                             choices = list("Automatic"=0, "Manual"=1),
+                             selected = 1),
+                checkboxGroupInput("cyl", label = "Number of Cyclinders",
+                                   choices = list("4"=4, "6"=6, "8"=8),
+                                   selected = 4)
+            )
+        ),
+        
+        # Show a plot of the generated distribution
+        mainPanel(
+
+            tabsetPanel(
+                tabPanel("Plot", plotOutput("distPlot")),
+                tabPanel("Data", tableOutput("table"))
+            )
+        )
     )
-  )
+))
