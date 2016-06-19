3030f42bde5d68372586c3957d79512c47cd62e2
WPShen53 <WPShen53@users.noreply.github.com>
2016-06-19 15:35:06 +0800

    Update booking.r

--- DF/booking.r
+++ DF/booking.r
@@ -2,53 +2,63 @@ library(dplyr)
 library(lubridate)
 
 FillMissingMonth <- function (tm, dur) {
-  missing_mon <- dur[dur<max(tm$BOOK_MONTH) & 
-                       dur>min(tm$BOOK_MONTH) & 
-                       !(dur%in%tm$BOOK_MONTH)]
-  cus <- tm$Customer.Family[1]
-  tm6 <- tm$TM6[1]
-  if (length(missing_mon)>0) {
-    newrow <- tm[1:length(missing_mon),]
-    newrow <- mutate(newrow, Customer.Family=cus, TM6=tm6, QTY12=0, BOOK_MONTH=missing_mon)
-    tm <- rbind(tm, newrow)
-  }
-  tm <- mutate(tm, MonDif = CalMonthDif(BOOK_MONTH))
-  tm <- tm[order(tm$MonDif),]
-  tm
+    missing_mon <- dur[dur<max(tm$BOOK_MONTH) & 
+                           dur>min(tm$BOOK_MONTH) & 
+                           !(dur%in%tm$BOOK_MONTH)]
+    if (length(missing_mon)>0) {
+        newrow <- tm[1:length(missing_mon),]
+        newrow <- mutate(newrow, QTY12=0, BOOK_MONTH=missing_mon)
+        tm <- rbind(tm, newrow)
+    }
+    tm <- mutate(tm, MonDif = CalMonthDif(BOOK_MONTH))
+    tm <- tm[order(tm$MonDif),]
+    tm
 }
 
 CalMonthDif <- function(month) {
-  start <- month[1]
-  dif <- month - start
-  for (i in 1:7) {
-    dif[which(dif>88)] <- dif[which(dif>88)] - 88
-  }
-  dif
+    start <- month[1]
+    dif <- month - start
+    for (i in 1:7) {
+        dif[which(dif>88)] <- dif[which(dif>88)] - 88
+    }
+    dif
 }
 
 unfactorize <- function(df){
-  for(i in which(sapply(df, class) == "factor")) df[[i]] = as.character(df[[i]])
-  return(df)
+    for(i in which(sapply(df, class) == "factor")) df[[i]] = as.character(df[[i]])
+    return(df)
 }
 
 # df_booking_Qty <- read.csv("~/R/Project/df_booking_Qty.csv")
 
 load("~/R/Project/df_booking_Qty.RData")
 
+
+## Build the booking quantity in QTY12 for each TM6 and split into
+## a list of data.frame where TM6 is the names of list
+
 df <- subset(df_booking_Qty, select=-c(QTY8,SVC,Sector,Segment))
 sum(duplicated(df))
-df <- aggregate (QTY12 ~ Customer.Family + TM6 + BOOK_MONTH, data = df, sum)
-df <- df[order(df$Customer.Family, df$TM6),]
 dur <- unique(df$BOOK_MONTH)
-cus <- unique(df$Customer.Family)
-tm6 <- unique(df$TM6)
+df <- aggregate (QTY12 ~ TM6 + BOOK_MONTH, data = df, sum)
+df <- df[order(df$TM6),]
 
 tm6.list <- split (df, df$TM6)
+tm6.list <- lapply(tm6.list, FUN=subset, select=-TM6)
 tm6.list <- lapply(tm6.list, FUN=unfactorize)
 tm6.list <- lapply(tm6.list, FUN=FillMissingMonth, dur=dur)
 
+tms <- tm6.list[["TMS477"]] #TMS477, TMF472
+plot(x=tms$MonDif,y=tms$QTY12)
+lines(x=tms$MonDif,y=tms$QTY12)
 
-df_tm6 <- aggregate(QTY12 ~ Customer.Family + TM6, data = df, sum)
+
+## Build the summary data.frame which conten the 
+
+sum_df <- aggregate(QTY12 ~ Customer.Family + TM6, data = df_booking_Qty, sum)
+sum_df <- unfactorize(sum_df)
+cus <- unique(sum_df$Customer.Family)
+tm6 <- unique(sum_df$TM6)
 
 
 ### working script sets
