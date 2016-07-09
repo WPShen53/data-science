59733160ffb48dc201974a0d1bc8c1c003da8888
WPShen53 <WPShen53@users.noreply.github.com>
2016-07-09 17:25:14 +0800

    Update DAT201x.py

--- Python/DAT201x.py
+++ Python/DAT201x.py
@@ -4,6 +4,9 @@ Created on Sat Jul 09 14:57:41 2016
 
 @author: Wenping Shen
 """
+###
+### Manipulating Data
+###
 
 import pandas as pd
 
@@ -43,6 +46,11 @@ df.col0 < 0
 df[df.col0 <0]
 df[(df.col0<0)|(df.col1<0)]
 
+
+###
+### Feature Representation
+###
+
 ordered_satisfaction = ['Very Unhappy', 'Unhappy', 'Neutral', 'Happy', 'Very Happy']
 df = pd.DataFrame({'satisfaction':['Mad', 'Happy', 'Unhappy', 'Neutral']})
 df.satisfaction = df.satisfaction.astype("category",
@@ -73,16 +81,59 @@ from scipy import misc
 img = misc.imread('Course Golden Ratio.png')
 print type(img)
 print img.shape, img.dtype
+
 # gray scale img
 red = img[:,0]
 green = img[:,1]
 blue = img[:,2]
 gray = (0.299*red + 0.587*green + 0.114*blue)
+
 # Is the image too big? Shrink it by an order of magnitude
 img = img[::2, ::2]
+
 # Scale colors from (0-255) to (0-1), then reshape to 1D Array
 X = (img / 255.0).reshape(-1, 3)
 
+# For audio
 import scipy.io.wavfile as wavfile
 sample_rate, audio_data = wavfile.read('sound.wav')
 print audio_data
+
+
+###
+### Wrangling Data
+###
+
+df.vertebrates_Bird.isnull()
+df.notnull()
+
+df.vertebrates_Bird.fillna( df.my_feature.mean() )
+df.fillna(0)
+df.fillna(method='ffill')  # fill the values forward
+df.fillna(method='bfill')  # fill the values in reverse
+df.fillna(limit=5)
+
+df = df.dropna(axis=0)  # row 
+df = df.dropna(axis=1)  # column
+
+# Drop any row that has at least 4 NON-NaNs within it:
+df = df.dropna(axis=0, thresh=4)
+
+# Axis=1 for columns
+df = df.drop(labels=['Features', 'To', 'Delete'], axis=1)
+df = df.drop_duplicates(subset=['Feature_1', 'Feature_2'])
+# reindex after dropping the duplicates
+df = df.reset_index(drop=True) # drop=True remove the backup copy of original index
+# chain the above togather
+df = df.dropna(axis=0, thresh=2).drop(labels=['ColA'], axis=1)\
+     .drop_duplicates(subset=['ColB', 'ColC']).reset_index()
+
+print df.types
+df.Date = pd.to_datetime(df.Date, errors='coerce')
+df.Height = pd.to_numeric(df.Height, errors='coerce')
+df.Weight = pd.to_numeric(df.Weight, errors='coerce')
+df.Age = pd.to_numeric(df.Age, errors='coerce')
+print df.dtypes
+
+df.vertebrates_Bird.unique()
+df.vertebrates_Bird.value_counts()
